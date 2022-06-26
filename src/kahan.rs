6a91851db72ed0b610b45cc13305f554bccaa2f1
akmitrich <ak.mitrich@mail.ru>
2022-06-26 21:44:31 +0300

    Operator overloads

--- src/kahan.rs
+++ src/kahan.rs
@@ -1,3 +1,5 @@
+use std::ops::{AddAssign, Add};
+
 #[allow(unused, dead_code)]
 
 #[derive(Debug, Clone, Default)]
@@ -9,6 +11,22 @@ pub struct KahanSum {
 impl From<f32> for KahanSum {
     fn from(initial: f32) -> Self {
         KahanSum { sum: initial, err: 0_f32 }
+    }
+}
+
+impl AddAssign for KahanSum {
+    fn add_assign(&mut self, rhs: f32) {
+        self.add(rhs);
+    }
+}
+
+impl Add for KahanSum {
+    type Output = Self;
+
+    fn add(self, rhs: f32) -> Self::Output {
+        let mut instance = self;
+        instance += rhs;
+        instance
     }
 }
 
