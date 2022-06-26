5aeca8242cafe9b228011b84920cab7c14585497
akmitrich <ak.mitrich@mail.ru>
2022-06-26 21:36:59 +0300

    With video test

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,19 @@ pub mod kahan;
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn video_example() {
+        let mut ks = kahan::KahanSum::default();
+        ks.add(16777216_f32);
+        assert_eq!(16777216., ks.get_sum());
+        assert_eq!(0., ks.get_err());
+        ks.add(1_f32);
+        assert_eq!(16777216., ks.get_sum());
+        assert_eq!(-1., ks.get_err());
+        ks.add(1_f32);
+        assert_eq!(16777218., ks.get_sum());
+        assert_eq!(0., ks.get_err());
     }
 }
