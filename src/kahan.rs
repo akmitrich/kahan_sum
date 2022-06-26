2f2b37cd865ca0a080c45344061b199183f26af1
akmitrich <ak.mitrich@mail.ru>
2022-06-26 21:58:05 +0300

    Kahan Summator

--- src/kahan.rs
+++ src/kahan.rs
@@ -7,6 +7,27 @@ pub struct KahanSum {
     err: f32,
 }
 
+pub trait KahanSummator {
+    fn kahan_sum(self) -> KahanSum;
+}
+
+impl KahanSum {
+    pub fn get_sum(&self) -> f32 {
+        self.sum
+    }
+
+    pub fn get_err(&self) -> f32 {
+        self.err
+    }
+
+    pub fn add(&mut self, value: f32) {
+        let corrected = value - self.get_err();
+        let new_sum = self.get_sum() + corrected;
+        self.err = (new_sum - self.get_sum()) - corrected;
+        self.sum = new_sum;
+    }
+}
+
 impl From<f32> for KahanSum {
     fn from(initial: f32) -> Self {
         KahanSum {
@@ -32,19 +53,10 @@ impl Add<f32> for KahanSum {
     }
 }
 
-impl KahanSum {
-    pub fn get_sum(&self) -> f32 {
-        self.sum
-    }
-
-    pub fn get_err(&self) -> f32 {
-        self.err
-    }
-
-    pub fn add(&mut self, value: f32) {
-        let corrected = value - self.get_err();
-        let new_sum = self.get_sum() + corrected;
-        self.err = (new_sum - self.get_sum()) - corrected;
-        self.sum = new_sum;
+impl<It> KahanSummator for It
+where It: Iterator<Item = f32>
+{
+    fn kahan_sum(self) -> KahanSum {
+        self.fold(Default::default(), |sum, item| sum + item)
     }
 }
